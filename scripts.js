d872c6b8beda04eeac9e45c8329f201cbad17f55
souravhasan001 <beingsouravhasan@gmail.com>
2025-02-15 17:08:39 +0600

    Review section ccomplete

--- scripts.js
+++ scripts.js
@@ -32,3 +32,28 @@ let navbar = document.querySelector('.navbar');
         },
         loop:true,
       });
+
+      var swiper = new Swiper(".review-slider", {
+        spaceBetween: 20,
+        centeredSlides: true,
+        autoplay: {
+            delay: 5500,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1, 
+            },
+            640: {
+                slidesPerView: 2, 
+            },
+            768: {
+                slidesPerView: 2, 
+            },
+            1024: {
+                slidesPerView: 3, 
+            },
+        },
+    });
+    
